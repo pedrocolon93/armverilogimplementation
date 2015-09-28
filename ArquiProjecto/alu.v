873b0378a82c4aa36827f48f20cfa7297746787d
Pedro <pedro.colon4@upr.edu>
2015-09-27 23:45:48 -0400

    Initial alu done.  Need to test/verify

--- ArquiProjecto/alu.v
+++ ArquiProjecto/alu.v
@@ -1,39 +1,321 @@
-module ALU(output reg [31:0]ALU_OUTPUT, output reg COUT, V, input  [31:0]LEFT_OP, RIGHT_OP, input  [3:0]FN, input  CIN);
+module ALU(output reg [31:0]ALU_OUTPUT, output reg Z,N,C, V, input  [31:0]LEFT_OP, RIGHT_OP, input  [3:0]FN, input  CIN);
 	always @(LEFT_OP, RIGHT_OP, FN, CIN)
 		begin
 			case(FN)
 			//AND
-			4'b0000: ALU_OUTPUT[31:0] = LEFT_OP[31:0] && RIGHT_OP[31:0];
+			4'b0000: 
+			begin
+				//Set the output and C flag
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] && RIGHT_OP[31:0];
+				
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+
+
+			end
 			// //EOR
-			4'b0001: ALU_OUTPUT[31:0] = LEFT_OP[31:0] ^ RIGHT_OP[31:0];
+			4'b0001: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] ^ RIGHT_OP[31:0];
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //SUB
-			4'b0010: ALU_OUTPUT[31:0] = LEFT_OP[31:0] -  RIGHT_OP[31:0];
+			4'b0010: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] -  RIGHT_OP[31:0];
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //RSB
-			4'b0011: ALU_OUTPUT[31:0] = RIGHT_OP[31:0]-  LEFT_OP[31:0] ;
+			4'b0011: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = RIGHT_OP[31:0]-  LEFT_OP[31:0];
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //ADD
-			4'b0100: ALU_OUTPUT[31:0] = LEFT_OP[31:0] +  RIGHT_OP[31:0];
+			4'b0100: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] +  RIGHT_OP[31:0];
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //ADC
-			// 4'b0101:
+			4'b0101: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] + RIGHT_OP[31:0] + CIN;
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //SBC
-			// 4'b0110:
+			4'b0110: 
+			begin
+				{C,ALU_OUTPUT[31:0]} = LEFT_OP[31:0] - RIGHT_OP[31:0] + CIN - 1;
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //RSC
-			// 4'b0111:
+			4'b0111: 
+			begin
+				{C,ALU_OUTPUT[31:0]} =  RIGHT_OP[31:0] - LEFT_OP[31:0] + CIN - 1;
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //TST
-			// 4'b1000:
+			4'b1000: 
+			begin
+				{C} = LEFT_OP[31:0] && RIGHT_OP[31:0];
+
+				if(LEFT_OP[31:0] && RIGHT_OP[31:0]==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=(LEFT_OP[31:0] && RIGHT_OP[31:0]))
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //TEQ
-			// 4'b1001:
+			4'b1001:
+			begin 
+				{C} = LEFT_OP[31:0] ^ RIGHT_OP[31:0];
+				
+				N =  LEFT_OP[31] ^ RIGHT_OP[31];
+
+				if(LEFT_OP[31:0] ^ RIGHT_OP[31:0]==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=(LEFT_OP[31] ^ RIGHT_OP[31]))
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //CMP
-			// 4'b1010:
+			4'b1010:
+			begin 
+				{C} = LEFT_OP[31:0] -  RIGHT_OP[31:0];
+				//Set the N flag
+				N = (LEFT_OP[31:0] -  RIGHT_OP[31:0]);
+				//Set the Z flag
+				if((LEFT_OP[31:0] -  RIGHT_OP[31:0])==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=(LEFT_OP[31:0] -  RIGHT_OP[31:0]))
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //CMN
-			// 4'b1011:
+			4'b1011:
+			begin
+				{C} = LEFT_OP[31:0] +  RIGHT_OP[31:0];
+				N = LEFT_OP[31:0] +  RIGHT_OP[31:0];
+				//Set the Z flag
+				if((LEFT_OP[31:0] +  RIGHT_OP[31:0])==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=(LEFT_OP[31:0] +  RIGHT_OP[31:0]))
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //ORR
-			// 4'b1100:
+			4'b1100: 
+			begin
+				ALU_OUTPUT[31:0] = LEFT_OP[31:0] || RIGHT_OP[31:0];
+			end
 			// //MOV
-			// 4'b1101:
+			4'b1101:
+			begin
+			 	ALU_OUTPUT[31:0] = RIGHT_OP[31:0];
+
+			 	//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //BIC
-			// 4'b1110:
+			4'b1110: 
+			begin
+				ALU_OUTPUT[31:0] = LEFT_OP[31:0] && !RIGHT_OP[31:0];
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 			// //MVN
-			// 4'b1111:
+			4'b1111: 
+			begin
+				ALU_OUTPUT[31:0] = !RIGHT_OP[31:0];
+				//Set the N flag
+				N = ALU_OUTPUT[31];
+				//Set the Z flag
+				if(ALU_OUTPUT==0)
+					Z = 1;
+				else
+					Z = 0;
+				//Set the overflow flag
+				//Check for 2's complement overflow
+				if((LEFT_OP[31]==RIGHT_OP[31]))
+					if(LEFT_OP[31]!=ALU_OUTPUT[31])
+						V=1;
+					else
+						V=0;
+				else 
+					V=0;
+			end
 
 			endcase // FN
 		end
--- ArquiProjecto/module test_alu.v
+++ ArquiProjecto/module test_alu.v
@@ -5,24 +5,24 @@ reg [3:0] IF; //Function select input
 reg CIN; //Carry in input
 //OUTPUTS
 wire [31:0] ALUOUT;
-wire COUT, V; //Las salidas deben ser tipo wire
+wire COUT, V, ZERO, N; //Las salidas deben ser tipo wire
 parameter sim_time = 160;
-ALU alu1(ALUOUT, COUT, V, IL, IR, IF, CIN); // Instanciación del módulo
+ALU alu1(ALUOUT, ZERO, N, COUT, V, IL, IR, IF, CIN); // Instanciación del módulo
 
 initial #sim_time $finish; // Especifica cuando termina simulación
 
 initial begin
 IF = 4'b0000; //Genera las combinaciones de las entradas
-IL = 32'h0000000f;
-IR = 32'h0000000f;
+IL = 32'h00000001;
+IR = 32'h00000001;
 CIN = 1'b0;
 
 repeat (15) #10 IF = IF + 4'b0001;
 end
 
 initial begin
-$display (" IF 			IL 			IL 			ALUOUT"); //imprime header
-$monitor ("   %b%b%b%b        %h %h %h", IF[3], IF[2], IF[1], IF[0], IL ,IR, ALUOUT); //imprime las señales
+$display (" IF 	  C N V Z 		   IL 	  IL 	  ALUOUT"); //imprime header
+$monitor ("   %b%b%b%b  %b %b %b %b         %h %h %h", IF[3], IF[2], IF[1], IF[0], COUT, N, V, ZERO, IL ,IR, ALUOUT); //imprime las señales
 end
 
 endmodule
