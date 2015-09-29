50d07db3fb3e921cf9e16f91f45b91cafc0275ac
marie <rookyann@gmail.com>
2015-09-29 14:23:38 -0400

    test to prove something

--- ArquiProjecto/ram_512x8.v
+++ ArquiProjecto/ram_512x8.v
@@ -1,98 +1,85 @@
-//MAS: 00 B  //  01 H  //  10 w  // 11 undefined
-// need to check for real values ---^
-// 1 = read  //  0 = write
+/*MAS: 00 B  //  01 H  //  10 w  // 11 undefined
+1 = read  //  0 = write
+changed mem value for test purposes */
 
-module ram512x8 (output reg [31:0]dataOut, done, input enable, readWrite, input [8:0]address, input [31:0]dataIn, input [1:0]MAS, input [1:0]A);
-	reg [31:0]mem[0:511];
-	always @ (enable, readWrite, sel)
+module ram512x8 (output reg [31:0]dataOut, output reg done, input enable, readWrite, input [8:0]address, input [31:0]dataIn, input [1:0]MAS, A);
+	reg [8:0]mem[0:20];
+	always @ (enable, readWrite, MAS, dataIn, A, address)
 	begin
 		if (enable)
 		begin
-			done = 1'b0;
-			if (readWrite)
-			begin
+			done = 0;
+			if (readWrite) begin
+				//Reading
 				case(MAS)
-					2'b00:
-					begin
-						case(A):
-							2'b00:	dataOut[7:0] = mem[address + 8b0000011];
-							2'b01:	dataOut[7:0] = mem[address + 8b0000010];
-							2'b10:	dataOut[7:0] = mem[address + 8b0000001];
-							2'b11:	dataOut[7:0] = mem[address];
+					2'b00:	begin
+						case(A)
+							2'b00:	dataOut[7:0] = mem[address + 8'b0000011][7:0];
+							2'b01:	dataOut[7:0] = mem[address + 8'b0000010][7:0];
+							2'b10:	dataOut[7:0] = mem[address + 8'b0000001][7:0];
+							2'b11:	dataOut[7:0] = mem[address][7:0];
 						endcase
 						dataOut[31:8] = 24'b0000_0000_0000_0000_0000_0000;
-						#15;
+						// #15;
 					end
-					2'b01:	
-					begin	
-						case(A[1]):
-							1'b0:
-							begin
-								dataOut[15:8] = mem[address + 8b0000010];
-								dataOut[7:0] = mem[address + 8b0000011];
+					2'b01:	begin	
+						case(A[1])
+							1'b0:	begin
+								dataOut[15:8] = mem[address + 8'b0000010][7:0];
+								dataOut[7:0] = mem[address + 8'b0000011][7:0];
 							end
-							1'b1:
-							begin
-								dataOut[15:8] = mem[address];
-								dataOut[7:0] = mem[address + 8b0000001];
+							1'b1:	begin
+								dataOut[15:8] = mem[address][7:0];
+								dataOut[7:0] = mem[address + 8'b0000001][7:0];
 							end
 						endcase
 						dataOut[31:16] = 16'b0000_0000_0000_0000;
-						#20;
+						// #20;
 					end
-					2'b10:	
-					begin	
-						dataOut[31:0] = {mem[address], mem[address + 8b0000001], mem[address + 8b0000010], mem[address + 8b0000011]};
-						#30;
+					2'b10:	begin	
+						dataOut[31:0] = {mem[address][7:0], 
+										 mem[address + 8'b0000001][7:0], 
+										 mem[address + 8'b0000010][7:0], 
+										 mem[address + 8'b0000011][7:0]};
+						// #30;
 					end
-					
-					//2'b11:    see what to do here
-				
 				endcase
 			end
-			else
-			begin
-				case(sel)
-					2'b00:	
-					begin	
-						case(A):
-							2'b00:	mem[address + 8b0000011] = dataIn[7:0];
-							2'b01:	mem[address + 8b0000010] = dataIn[7:0];
-							2'b10:	mem[address + 8b0000001] = dataIn[7:0];
-							2'b11:	mem[address] = dataIn[7:0];
+			else begin
+				//Writing
+				case(MAS)
+					2'b00:	begin	
+						case(A)
+							2'b00:	mem[address + 8'b0000011][7:0] = dataIn[7:0];
+							2'b01:	mem[address + 8'b0000010][7:0] = dataIn[7:0];
+							2'b10:	mem[address + 8'b0000001][7:0] = dataIn[7:0];
+							2'b11:	mem[address][7:0] = dataIn[7:0];
 						endcase
-						#25;
+						// #25;
 					end
-					2'b01:	
-					begin
-						case(A[1]):
-							1'b0:
-							begin
-								mem[address + 8b0000010] = dataOut[15:8];
-								mem[address + 8b0000011] = dataOut[7:0];
+					2'b01:	begin
+						case(A[1])
+							1'b0:	begin
+								mem[address + 8'b0000010][7:0] = dataIn[15:8];
+								mem[address + 8'b0000011][7:0] = dataIn[7:0];
 							end
-							1'b1:
-							begin
-								mem[address] = dataOut[15:8] ;
-								mem[address + 8b0000001] = dataOut[7:0] ;
+							1'b1:	begin
+								mem[address][7:0] = dataIn[15:8];
+								mem[address + 8'b0000001][7:0] = dataIn[7:0] ;
 							end
 						endcase
-						#35;
+						//#35;
 					end
-					2'b10:	
-					begin	
-						mem[address + 8'b00000011] = dataIn[7:0];
-						mem[address + 8'b00000010] = dataIn[15:8];
-						mem[address + 8'b00000001] = dataIn[23:16];
-						mem[address] = dataIn[31:24];
-						#60;
+					2'b10:	begin	
+						mem[address + 8'b00000011][7:0] = dataIn[7:0];
+						mem[address + 8'b00000010][7:0] = dataIn[15:8];
+						mem[address + 8'b00000001][7:0] = dataIn[23:16];
+						mem[address][7:0] = dataIn[31:24];
+						//#60;
 					end
-					
-					//2'b11:
-				
 				endcase
 			end
-			done = 1'b1;
+			done = 1;
 		end
 		else
 			dataOut = 32'bz;
--- ArquiProjecto/ram_512x8_test.v
+++ ArquiProjecto/ram_512x8_test.v
@@ -1,8 +1,8 @@
 module ram_test;
 	reg en;
+	reg rw;
 	reg [8:0]adr;
 	reg [31:0]data;
-	reg [1:0]rw;
 	reg [1:0]dataSize;
 	reg [1:0]dataPlace;
 
@@ -11,35 +11,52 @@ module ram_test;
 
 	ram512x8 ram(out, finished, en, rw, adr, data, dataSize, dataPlace);
 
+	parameter sim_time = 1500;
+	initial #sim_time $finish;
+
 	initial begin
-		rw = 0'b0;
-		adr = 8'b0010000;
-		data = 32'h0000000f;
+		en = 1'b0;
+		rw = 1'b0;
+		adr = 8'b0000000;
+		data = 32'h00000002;
 		dataSize = 2'b00;
 		dataPlace = 2'b11;
 
-		en = 1'b1;
-		rw = 0'b0;
-		adr = 8'b0010000;
-		data = 32'h00000002;
-		dataSize = 2'b00;
-		dataPlace = 2'b10;
+		#1 repeat (3) #10 begin  //write B
+			en = 1'b1;
+			data = data + 1;
+			dataPlace = dataPlace - 2'b01;
+		end
+		#40 repeat(4) #15 begin		//read B
+			rw = 1'b1;
+			dataPlace = dataPlace + 2'b01;
+		end
+		
 
-		rw = 0'b0;
-		adr = 8'b0010000;
-		data = 32'h00000004;
-		dataSize = 2'b00;
-		dataPlace = 2'b01;
+		#65 repeat(2) #15 begin		//write H
+			rw = 1'b0;
+			dataSize = 2'b01;
+			data = data + 1;
+			dataPlace = dataPlace - 2'b10;
+		end
+		#130 repeat(2) #15 begin		//read H
+			rw = 1'b1;
+			dataPlace = dataPlace + 2'b10;
+		end
 
-		rw = 0'b0;
-		adr = 8'b0010000;
-		data = 32'h00000008;
-		dataSize = 2'b00;
-		dataPlace = 2'b00;
+
+		#200 repeat(1) #15 begin		//write W
+			rw = 1'b0;
+			dataSize = 2'b10;
+			data = data + 1;
+		end
+		#265 repeat(1) #15 begin		//read W
+			rw = 1'b1;
+		end
 	end
 
 	initial begin
-		$display("data");
-		$monitor("%h", out);
+		$display("data     size dP RW EN");
+		$monitor("%h %b   %b %b  %b", out, dataSize, dataPlace, rw, en);
 	end
 endmodule
