53184b68c1d6c00c1c4976485ac6425fba8bf989
Hector Herandnez <hector.hernandez19@upr.edu>
2015-10-23 14:03:30 -0400

    Datapath Circuit with RF Muxes added. Also PC + 8 for Specific Case of Shifter Operand = R15

--- ArquiProjecto/Phase2/shifter2.v
+++ ArquiProjecto/Phase2/shifter2.v
@@ -11,7 +11,7 @@ module mux_2x1(output [31:0] Y, input S, input [31:0] I0, I1);
 	assign Y=S? I0:I1;
 endmodule
 
-module mux_2x1_2b(output [1:0] Y, input S, input [1:0] I0, I1);
+module mux_2x1_2b(output [2:0] Y, input S, input [2:0] I0, I1);
 	assign Y=S? I0:I1;
 endmodule
 
@@ -27,7 +27,7 @@ module internal_shifter (
 	input shift_type,
 	output reg [31:0] shift_out
 );
-	reg I;
+	reg I;	
 	always @(amount, value, shift_type) 
 	begin
 		case(shift_type)
@@ -48,13 +48,17 @@ module internal_shifter (
 				//right arithmetic
 				shift_out[31:0] = value[31:0]>>>amount;
 			end
+			// 4:begin
+			// 	//rotate right, amount = rotate_imm, value = immed_8
+			// 	shift_out[31:0] = value[31:0]>>>amount;
+			// end
 		endcase // shift_type
 	end
 endmodule
 
 module shifter(input[31:0] input_register, input[11:0] shifter_operand, input selector, output [31:0] out);
 	wire[31:0] amounttointernal,valuetointernal;
-	wire[1:0] shifttypetointernal;
+	wire[2:0] shifttypetointernal;
 	//
 	mux_2x1 amount_mux(amounttointernal,selector,{27'b0000_0000_0000_0000_0000_0000_000,shifter_operand[11:8],1'b0}, {27'b0000_0000_0000_0000_0000_0000_000,shifter_operand[11:7]});
 	mux_2x1 value_mux(valuetointernal,selector,{24'b0000_0000_0000_0000_0000_0000,shifter_operand[7:0]},input_register[31:0]);
